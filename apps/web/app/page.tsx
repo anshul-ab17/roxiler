0b6dca5c0de7a7fabce7d4af3810abf1f2148c34
Anshul Bharat <anshul.ab17x@gmail.com>
2026-02-20 16:10:09 +0530

    init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,6 @@
 import { prisma } from "@repo/database";
-
 export default async function IndexPage() {
   const users = await prisma.user.findMany();
-
   return (
     <div>
       <h1>Hello World</h1>
